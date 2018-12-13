afc56702f794845032dbf23c063553441cf46193
Steven Stef <sjstef@gamil.com>
2018-12-13 12:02:39 -0500

    initial commit

---
+++ automation.test.js
@@ -0,0 +1,9 @@
+const { Builder, Capabilities, By, Key, until } = require('selenium-webdriver')
+const chrome = require('selenium-webdriver/chrome')
+const { path } = require('chromedriver')
+
+describe('does this work', ()=> {
+  test('this does work', () => {
+    expect(1).toEqual(1)
+  })
+})
